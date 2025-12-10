391539372a35171f5184281eba38ab7905a208dd
rizkaputriananda7 <rizkaputriananda47@gmail.com>
2025-12-10 13:08:10 +0700

    Database News

--- models/index.js
+++ models/index.js
@@ -2,7 +2,20 @@ const sequelize = require('../config/db');
 const User = require('./user')(sequelize);
 const Bin = require('./bin')(sequelize);
 
+// Import models edukasi dari file gabungan
+const { MateriEdukasi, FileUpload, Notifikasi, UpdateMateriEdukasi, HapusMateriEdukasi } = require('./news');
+
+// User and Bin relationships
 User.hasMany(Bin, { foreignKey: 'userId' });
 Bin.belongsTo(User, { foreignKey: 'userId' });
 
-module.exports = { sequelize, User, Bin };
+module.exports = { 
+  sequelize, 
+  User, 
+  Bin, 
+  MateriEdukasi, 
+  FileUpload, 
+  Notifikasi, 
+  UpdateMateriEdukasi, 
+  HapusMateriEdukasi 
+};
